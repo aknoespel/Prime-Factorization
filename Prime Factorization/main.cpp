fe5ccbc2ac696c29ad5c49242a64915f4b2b0051
Alexander Knoespel <aknoespel@Alexanders-MacBook-Pro.local>
2014-12-22 16:02:22 -0600

    Initial Commit

---
+++ Prime Factorization/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Prime Factorization
+//
+//  Created by Alexander Knoespel on 12/22/14.
+//  Copyright (c) 2014 Alexander Knoespel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
